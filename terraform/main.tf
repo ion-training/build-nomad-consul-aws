6c69b21e9e7621bbb68008d6ceefec0da0b80769
ion <ion@hashicorp.com>
2021-11-09 12:07:23 +0100

    adjusted terraform

---
+++ terraform/main.tf
@@ -0,0 +1,193 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.64.2"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_vpc
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet_public" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.cidr_subnet
+}
+
+resource "aws_route_table" "rtb_public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta_subnet_public" {
+  subnet_id      = aws_subnet.subnet_public.id
+  route_table_id = aws_route_table.rtb_public.id
+}
+
+resource "aws_security_group" "sg_22_80" {
+  name   = "sg_22"
+  vpc_id = aws_vpc.vpc.id
+
+  # SSH access from the VPC
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# VARIABLES #
+#############
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default     = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default     = "10.1.0.0/24"
+}
+
+variable "environment_tag" {
+  description = "Environment tag"
+  default     = "Learn"
+}
+
+variable "region" {
+  description = "The region Terraform deploys your instance"
+  type        = string
+  default     = "eu-west-3"
+}
+
+variable "variablefrom-packer" {
+  description = "this variable is read form variablefrom-packer.tfvars"
+}
+
+# VARIABLES SSH KEY CREATE #
+############################
+variable "key_name" {
+  description = "ssh key name"
+  default     = "key-ssh"
+}
+
+resource "tls_private_key" "key-temporary" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.key-temporary.public_key_openssh
+
+
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.key-temporary.private_key_pem}' > ./id_rsa && chmod 400 id_rsa"
+  }
+
+}
+
+# filter AMI create instance #
+##############################
+
+data "aws_ami" "packer_image" {
+  most_recent = true
+
+  filter {
+    name   = "tag:Created-by"
+    values = ["Packer"]
+  }
+
+  filter {
+    name   = "tag:Version"
+    values = ["${var.variablefrom-packer}"]
+  }
+
+  owners = ["self"]
+}
+
+resource "random_pet" "rand-name" {
+  length = 1
+}
+
+resource "aws_instance" "server" {
+  count                       = 3
+  ami                         = data.aws_ami.packer_image.id
+  instance_type               = "t3.medium"
+  key_name                    = aws_key_pair.generated_key.key_name
+  subnet_id                   = aws_subnet.subnet_public.id
+  vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = "server-${random_pet.rand-name.id}"
+  }
+
+}
+
+
+output "how-to-connect-server0" {
+  value = "ssh -i id_rsa ubuntu@${aws_instance.server[0].public_ip}"
+}
+
+output "how-to-connect-server1" {
+  value = "ssh -i id_rsa ubuntu@${aws_instance.server[1].public_ip}"
+}
+
+output "how-to-connect-server2" {
+  value = "ssh -i id_rsa ubuntu@${aws_instance.server[2].public_ip}"
+}
+
+output "server0-privateIP" {
+  value = aws_instance.server[0].private_ip
+}
+
+output "server1-privateIP" {
+ value = aws_instance.server[1].private_ip
+}
+output "server2-privateIP" {
+ value = aws_instance.server[2].private_ip
+}
